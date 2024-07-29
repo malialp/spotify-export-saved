50e975af819b2ea44a8a182feb51c996c1ce361f
Muhammed Ali Alp <malibaba2234@gmail.com>
2024-07-30 00:48:28 +0300

    deleted old files

--- src/Components/Components.jsx
+++
@@ -1,6 +0,0 @@
-import Loading from "./Loading";
-import Buttons from "./Buttons";
-import LoginButton from "./LoginButton";
-import Footer from "./Footer";
-
-export { Loading, Buttons, LoginButton, Footer };
